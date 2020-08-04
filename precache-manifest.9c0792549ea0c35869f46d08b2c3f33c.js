8f999a06d41fec23aa59fc4394b9e5792b6e1ef2
clearink <1561939638@qq.com>
2020-08-05 07:38:11 +0800

    finish drawer bug

---
+++ precache-manifest.9c0792549ea0c35869f46d08b2c3f33c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abde96ce9d462634a98f2bac60397c38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b45df6bcd81e10f198c",
+    "url": "./static/css/2.86d5dbc7.chunk.css"
+  },
+  {
+    "revision": "88c38d1c088d4b16c640",
+    "url": "./static/css/main.4c5b30e3.chunk.css"
+  },
+  {
+    "revision": "3b45df6bcd81e10f198c",
+    "url": "./static/js/2.791f65e5.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "./static/js/2.791f65e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c38d1c088d4b16c640",
+    "url": "./static/js/main.9058271a.chunk.js"
+  },
+  {
+    "revision": "7166c849e55a6569a516",
+    "url": "./static/js/runtime-main.29840342.js"
+  }
+]);
